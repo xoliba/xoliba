a1a9285c4a0c0784f9c2575c257eb27c7d9d6958
Eero Pukkila <eeropukkila@hotmail.com>
2017-05-22 13:55:32 +0300

    validating moves 3

--- public/scripts/Logic.js
+++ public/scripts/Logic.js
@@ -5,5 +5,18 @@ function setGameboard(gameBoard){
 }
 
 function validateMove(firstX, firstY, secondX, secondY){
+  var whites = stonesBetweenAreWhite(firstX, firstY, secondX, secondY);
 
+}
+
+function stonesBetweenAreWhite(firstX, firstY, secondX, secondY){
+  if(firstX == secondX){
+    var min = Math.min(firstY, secondY);
+    var max = Math.max(firstY, secondY);
+    for (var i = min; i < max; i++) {
+      if(gameboard[firstX][i] != 0){
+        return false;
+      }
+    }
+  }
 }
