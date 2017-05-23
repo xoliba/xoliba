895e3aafb83045a361f44ededed1ed00a1a697df
rajaro <jaro.ravila@gmail.com>
2017-05-23 16:26:49 +0300

    vilin refactoilu

--- public/scripts/Launcher.js
+++ public/scripts/Launcher.js
@@ -13,107 +13,118 @@ function start() {
 
     setGameboard(stonesArray);
 
-    var firstClicked;
 
+    var firstClicked;
+    var sprites = [];
+    for (var i = 0; i < 7; i++) {
+        var spritesY = []
+        sprites.push(spritesY);
+    }
     var padding = size / 10;
     var px = size / 7.5;
     var radius = px / 4;
     var highlightScaling = radius / 100;
 
-      PIXI.loader
-      .add([
-          "images/whiteCircle64.png",
-          "images/blueCircle64.png",
-          "images/redCircle64.png"
-      ])
-      .load(setup);
+    PIXI.loader
+            .add([
+                "images/whiteCircle64.png",
+                "images/blueCircle64.png",
+                "images/redCircle64.png"
+            ])
+            .load(setup);
 
-      function setup() {
+
+    function setup() {
         for (var i = 0; i < 7; i++) {
-          for (var j = 0; j < 7; j++) {
-            if(!((i == 0 || i == 6) && (j == 0 || j == 6))){
+            for (var j = 0; j < 7; j++) {
+                if (!((i == 0 || i == 6) && (j == 0 || j == 6))) {
 
-              var sprite;
+                    var sprite;
 
-              let c = stonesArray[i][j];
-              let path = "";
-              if (c == -1) {
-                  path = "images/blueCircle64.png";
-              } else if (c == 0) {
-                  path = "images/whiteCircle64.png";
-              } else if (c == 1) {
-                  path = "images/redCircle64.png";
-              }
-
-              sprite = new PIXI.Sprite(
-                PIXI.loader.resources[path].texture
-              );
-
-              sprite.interactive = true;
-              sprite.buttonMode = true;
-              sprite.anchor.x = 0.5;
-              sprite.anchor.y = 0.5;
-              sprite.x = padding + i * px;
-              sprite.y = padding + j * px;
-              sprite.width = radius * 2;
-              sprite.height = radius * 2;
-
-
-              sprite.on('pointerdown', onPointerDown)
-
-              function onPointerDown(){
-                var image = this.texture.baseTexture.source.src.split("/").pop();
-                  if(firstClicked === undefined){
-                    if(image == "whiteCircle64.png"){
-                      return;
-                    }
-                    firstClicked = this;
-                    this.scale.x += highlightScaling;
-                    this.scale.y += highlightScaling;
-                  } else if(image == "whiteCircle64.png"){
-
-                    var firstX = Math.round((firstClicked.x - padding) / px);
-                    var firstY = Math.round((firstClicked.y - padding) / px);
-                    var secondX = Math.round((this.x - padding) / px);
-                    var secondY = Math.round((this.y - padding) / px);
-
-
-                    if (!validateMove(firstX, firstY, secondX, secondY)){
-                      return;
+                    let c = stonesArray[i][j];
+                    let path = "";
+                    if (c == -1) {
+                        path = "images/blueCircle64.png";
+                    } else if (c == 0) {
+                        path = "images/whiteCircle64.png";
+                    } else if (c == 1) {
+                        path = "images/redCircle64.png";
                     }
 
-                    firstClicked.scale.x -= highlightScaling;
-                    firstClicked.scale.y -= highlightScaling;
-                    var helpx = firstClicked.x;
-                    var helpy = firstClicked.y;
-                    firstClicked.x = this.x;
-                    firstClicked.y = this.y;
-                    this.x = helpx;
-                    this.y = helpy;
-                    firstClicked = undefined;
+                    sprite = new PIXI.Sprite(
+                            PIXI.loader.resources[path].texture
+                            );
 
-                    var help = stonesArray[firstX][firstY];
-                    stonesArray[firstX][firstY] = stonesArray[secondX][secondY];
-                    stonesArray[secondX][secondY] = help;
+                    sprite.interactive = true;
+                    sprite.buttonMode = true;
+                    sprite.anchor.x = 0.5;
+                    sprite.anchor.y = 0.5;
+                    sprite.x = padding + i * px;
+                    sprite.y = padding + j * px;
+                    sprite.width = radius * 2;
+                    sprite.height = radius * 2;
 
-                    console.log(trianglesFound(secondX, secondY))
+                    sprite.on('pointerdown', onPointerDown)
 
-                  } else if(firstClicked.x == this.x && firstClicked.y == this.y){
-                    firstClicked.scale.x -= highlightScaling;
-                    firstClicked.scale.y -= highlightScaling;
-                    firstClicked = undefined;
-                    return;
-                  }
-              }
+                    sprites[i].push(sprite);
 
-              app.stage.addChild(sprite);
+                    function onPointerDown() {
+                        var image = this.texture.baseTexture.source.src.split("/").pop();
+                        if (firstClicked === undefined) {
+                            if (image == "whiteCircle64.png") {
+                                return;
+                            }
+                            firstClicked = this;
+                            this.scale.x += highlightScaling;
+                            this.scale.y += highlightScaling;
+                        } else if (image == "whiteCircle64.png") {
 
+                            var firstX = Math.round((firstClicked.x - padding) / px);
+                            var firstY = Math.round((firstClicked.y - padding) / px);
+                            var secondX = Math.round((this.x - padding) / px);
+                            var secondY = Math.round((this.y - padding) / px);
+
+
+                            if (!validateMove(firstX, firstY, secondX, secondY)) {
+                                return;
+                            }
+
+                            firstClicked.scale.x -= highlightScaling;
+                            firstClicked.scale.y -= highlightScaling;
+                            var helpx = firstClicked.x;
+                            var helpy = firstClicked.y;
+                            firstClicked.x = this.x;
+                            firstClicked.y = this.y;
+                            this.x = helpx;
+                            this.y = helpy;
+                            firstClicked = undefined;
+
+                            var help = stonesArray[firstX][firstY];
+                            stonesArray[firstX][firstY] = stonesArray[secondX][secondY];
+                            stonesArray[secondX][secondY] = help;
+
+                            var spriteHelp = sprites[firstX][firstY];
+                            sprites[firstX][firstY] = sprites[secondX][secondY];
+                            sprites[secondX][secondY] = spriteHelp;
+
+                            console.log(trianglesFound(secondX, secondY))
+
+                        } else if (firstClicked.x == this.x && firstClicked.y == this.y) {
+                            firstClicked.scale.x -= highlightScaling;
+                            firstClicked.scale.y -= highlightScaling;
+                            firstClicked = undefined;
+                            return;
+                        }
+                    }
+
+                    app.stage.addChild(sprite);
+
+                }
             }
-          }
         }
-      }
+    }
 
 
 
-      app.renderer.render(app.stage);
+    app.renderer.render(app.stage);
 }
--- public/scripts/Logic.js
+++ public/scripts/Logic.js
@@ -1,151 +1,158 @@
 var gameboard;
 
-function setGameboard(gameBoard){
-  gameboard = gameBoard;
+function setGameboard(gameBoard) {
+    gameboard = gameBoard;
 }
 
-function validateMove(firstX, firstY, secondX, secondY){
-  if(!stonesBetweenAreWhite(firstX, firstY, secondX, secondY)){
-    return false;
-  } else if (!stonesAreOnTheSameLine(firstX, firstY, secondX, secondY)){
-    return false;
-  }
-  return true;
+function validateMove(firstX, firstY, secondX, secondY) {
+    if (!stonesBetweenAreWhite(firstX, firstY, secondX, secondY)) {
+        return false;
+    } else if (!stonesAreOnTheSameLine(firstX, firstY, secondX, secondY)) {
+        return false;
+    }
+    hitStones(triangle[0], triangle[1], triangle[2], triangle[3], triangle[4], triangle[5]);
+    return true;
 }
 
 function stonesBetweenAreWhite(firstX, firstY, secondX, secondY) {
-  if(firstX == secondX){
-    var min = Math.min(firstY, secondY);
-    var max = Math.max(firstY, secondY);
-    for (var i = min+1; i < max; i++) {
-      if(gameboard[firstX][i] != 0){
-        return false;
-      }
-    }
-  }
-  else if (firstY == secondY) {
-    var min = Math.min(firstX, secondX);
-    var max = Math.max(firstX, secondX);
-    for (var i = min+1; i < max; i++) {
-      if(gameboard[i][firstY] != 0) {
-        return false;
-      }
-
-    }
-  }
-  else if ((firstX > secondX && firstY > secondY) || (firstX < secondX && firstY < secondY)) {
-      var minX = Math.min(firstX, secondX);
-      var maxX = Math.max(firstX, secondX);
-      var minY = Math.min(firstY, secondY);
-      for (var i = minX+1; i < maxX; i++) {
-        minY++;
-        if (gameboard[i][minY] != 0) {
-          console.log(i, minY, gameboard[i][minY]);
-
-          return false;
+    if (firstX == secondX) {
+        var min = Math.min(firstY, secondY);
+        var max = Math.max(firstY, secondY);
+        for (var i = min + 1; i < max; i++) {
+            if (gameboard[firstX][i] != 0) {
+                return false;
+            }
         }
+    } else if (firstY == secondY) {
+        var min = Math.min(firstX, secondX);
+        var max = Math.max(firstX, secondX);
+        for (var i = min + 1; i < max; i++) {
+            if (gameboard[i][firstY] != 0) {
+                return false;
+            }
 
-      }
-  } else {
-      var minX = Math.min(firstX, secondX);
-      var maxX = Math.max(firstX, secondX);
-      var minY = Math.max(firstY, secondY);
-      for (var i = minX+1; i < maxX; i++) {
-        minY--;
-        if (gameboard[i][minY] != 0) {
-          console.log(i, minY, gameboard[i][minY]);
-
-          return false;
         }
+    } else if ((firstX > secondX && firstY > secondY) || (firstX < secondX && firstY < secondY)) {
+        var minX = Math.min(firstX, secondX);
+        var maxX = Math.max(firstX, secondX);
+        var minY = Math.min(firstY, secondY);
+        for (var i = minX + 1; i < maxX; i++) {
+            minY++;
+            if (gameboard[i][minY] != 0) {
+                console.log(i, minY, gameboard[i][minY]);
 
-      }
-  }
+                return false;
+            }
+
+        }
+    } else {
+        var minX = Math.min(firstX, secondX);
+        var maxX = Math.max(firstX, secondX);
+        var minY = Math.max(firstY, secondY);
+        for (var i = minX + 1; i < maxX; i++) {
+            minY--;
+            if (gameboard[i][minY] != 0) {
+                console.log(i, minY, gameboard[i][minY]);
+
+                return false;
+            }
+
+        }
+    }
 
 
-  return true;
+    return true;
 }
 
 function stonesAreOnTheSameLine(firstX, firstY, secondX, secondY) {
-  var diffX = Math.abs(firstX - secondX);
-  var diffY = Math.abs(firstY - secondY);
-  return firstX == secondX || firstY == secondY || diffX == diffY;
+    var diffX = Math.abs(firstX - secondX);
+    var diffY = Math.abs(firstY - secondY);
+    return firstX == secondX || firstY == secondY || diffX == diffY;
 }
 
+var triangle = []
 function trianglesFound(positionX, positionY) {
-  var color = gameboard[positionX][positionY];
-  var found = 0;
-  var foundOnThisDirection = 0;
-  for (var i = 2; i <= 6; i += 2) {
-    try {
-      foundOnThisDirection = 0;
-      if (gameboard[positionX + i][positionY] == color) {
-        foundOnThisDirection += checkDiagonals(positionX, positionY, i/2, i/2, i/2, -1 * i/2, color);
-        if(foundOnThisDirection == 2){
-          foundOnThisDirection++;
+    var color = gameboard[positionX][positionY];
+    var found = 0;
+    var foundOnThisDirection = 0;
+    let hypotenuseDirections = [[-1,0],[1,0],[0,-1],[0,1]]; //left, right, down, up
+    let edgeDirections = [[-1,1,-1,-1],[1,1,1,-1],[-1,-1,1,-1],[-1,1,1,1]] //left, right, down, up
+    
+    for (var i = 2; i <= 6; i += 2) { //for all possible triangle distances
+        let distanceSide = i / 2;
+        for (var j = 0; j < 4; j++) { //for all four directions
+            found += lookForTriangles(positionX, positionY, hypotenuseDirections[j][0] * i, hypotenuseDirections[j][1] * i, edgeDirections[j][0] * distanceSide, edgeDirections[j][1] * distanceSide,edgeDirections[j][2] * distanceSide,edgeDirections[j][3] * distanceSide, color)
         }
-        found += foundOnThisDirection;
-      } else if (checkDiagonals(positionX, positionY, i/2, i/2, i/2, -1 * i/2, color) == 2){
-        found++;
-      }
-      console.log(positionX + i, positionY, foundOnThisDirection, found);
+    }
+    return found;
+}
+
+function lookForTriangles(originX, originY, directionX, directionY, firstChangeX, firstChangeY, secondChangeX, secondChangeY, color) {
+    let foundOnThisDirection = 0;
+    try {
+        foundOnThisDirection = 0;
+        if (gameboard[originX + directionX][originY + directionY] == color) {
+            foundOnThisDirection += checkDiagonals(originX, originY, firstChangeX, firstChangeY, secondChangeX, secondChangeY, color);
+            if (foundOnThisDirection == 2) {
+                foundOnThisDirection++;
+            }
+        } else if (checkDiagonals(originX, originY, firstChangeX, firstChangeY, secondChangeX, secondChangeY, color) == 2) {
+            foundOnThisDirection++;
+        }
+        console.log("for stone (" + originX + "," + originY + ") to the direction of stone (" + (originX + directionX) + "," + (originY + directionY) + ") "
+            + "found " + foundOnThisDirection + " triangles");
     } catch (e) {
     }
-
-    try{
-      foundOnThisDirection = 0;
-      if (gameboard[positionX - i][positionY] == color) {
-        foundOnThisDirection += checkDiagonals(positionX, positionY, -1 * i/2, i/2, -1 * i/2, -1 * i/2, color);
-        if(foundOnThisDirection == 2){
-          foundOnThisDirection++;
-        }
-        found += foundOnThisDirection;
-      } else if (checkDiagonals(positionX, positionY, -1 * i/2, i/2, -1 * i/2, -1 * i/2, color) == 2){
-        found++;
-      }
-      console.log(positionX - i, positionY, foundOnThisDirection, found)
-    } catch (e){
-    }
-
-    try{
-      foundOnThisDirection = 0;
-      if (gameboard[positionX][positionY + i] == color) {
-        foundOnThisDirection += checkDiagonals(positionX, positionY, i/2, i/2, -1 * i/2, i/2, color);
-        if(foundOnThisDirection == 2){
-          foundOnThisDirection++;
-        }
-        found += foundOnThisDirection;
-      } else if (checkDiagonals(positionX, positionY, i/2, i/2, -1 * i/2, i/2, color) == 2){
-        found++;
-      }
-      console.log(positionX, positionY + i, foundOnThisDirection, found)
-    } catch (e){
-    }
-
-    try{
-      foundOnThisDirection = 0;
-      if (gameboard[positionX][positionY - i] == color) {
-        foundOnThisDirection += checkDiagonals(positionX, positionY, i/2, -1 * i/2, -1 * i/2, -1 * i/2, color);
-        if(foundOnThisDirection == 2){
-          foundOnThisDirection++;
-        }
-        found += foundOnThisDirection;
-      } else if (checkDiagonals(positionX, positionY, i/2, i/2, i/2, -1 * i/2, color) == 2){
-        found++;
-      }
-      console.log(positionX, positionY - i, foundOnThisDirection, found)
-    } catch (e){
-    }
-  }
-  return found;
+    return foundOnThisDirection;
 }
 
 function checkDiagonals(positionX, positionY, firstChangeX, firstChangeY, secondChangeX, secondChangeY, color) {
-  var result = 0;
-  if (gameboard[positionX + firstChangeX][positionY + firstChangeY] == color) {
-    result++;
-  }
-  if (gameboard[positionX + secondChangeX][positionY + secondChangeY] == color) {
-    result++;
-  }
-  return result;
+    return checkDiagonal(positionX, positionY, firstChangeX, firstChangeY, color) + checkDiagonal(positionX, positionY, secondChangeX, secondChangeY, color);
 }
+
+function checkDiagonal(positionX, positionY, firstChangeX, firstChangeY, color) {
+    var result = 0;
+    if (gameboard[positionX + firstChangeX][positionY + firstChangeY] == color) {
+        result++;
+    }
+    return result;
+}
+
+function hitStones(firstX, firstY, secondX, secondY, thirdX, thirdY) {
+    if (firstX == secondX) {
+        hitVerticalTriangle(firstX, firstY, secondY, thirdX);
+    } else if (firstX == thirdX) {
+
+    } else if (secondX == thirdX) {
+
+    } else if (firstY == secondY) {
+
+    } else if (firstY == thirdY) {
+
+    } else if (secondY == thirdY) {
+
+    }
+}
+
+function hitVerticalTriangle(startX, firstY, secondY, endX) {
+    var min = Math.min(firstY, secondY);
+    var max = Math.max(firstY, secondY);
+    var n = 1;
+    if (endX < startX) {
+        n *= -1;
+        var help = startX;
+        startX = endX;
+        endX = help;
+    }
+    for (var i = startX; i < endX; i += n) {
+        for (var j = min; j < max; j++) {
+            if (j == firstY || j == secondY) {
+                continue;
+            }
+            gameboard[i][j] = 0;
+            sprites[i][j].setTexture("images/whiteCircle64.png");
+        }
+        min++;
+        max--;
+    }
+    }
