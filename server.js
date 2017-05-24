8324ba5713665ae4eee4baece3faf919560229bc
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-05-24 16:34:04 +0300

    added more eslint rules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,9 +9,9 @@ app.set('views', __dirname + '/public');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.render('index')
+    res.render('index');
 });
 
 app.listen(app.get('port'), function() {
-	console.log('Node running on port', app.get('port'));
+    console.log('Node running on port', app.get('port'));
 });
