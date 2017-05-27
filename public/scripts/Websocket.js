6418b81a0b52a6b000173080983735a00cf5ca25
Eero Pukkila <eeropukkila@hotmail.com>
2017-05-27 11:27:43 +0300

    Attempt to fix 'security error'

--- public/scripts/Websocket.js
+++ public/scripts/Websocket.js
@@ -2,7 +2,7 @@ var aisocket;
 
 class AiSocket {
     constructor() {
-        const server = 'ws://127.0.0.1:4567/echo';
+        const server = 'wss://127.0.0.1:4567/echo';
         aisocket = new WebSocket(server);
 
         aisocket.onmessage = function(event) {
