8b05b5c0cea664fec5c49da9e47bb86697ae3bc3
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-07 12:03:01 +0300

    made karma run test in both chrome and firefox

--- karma.conf.js
+++ karma.conf.js
@@ -8,16 +8,16 @@ module.exports = function(config) {
     files: [
       'node_modules/babel-polyfill/dist/polyfill.js',
       'test/**/*.js'
-      //'public/**/*.js',
-      //'public/**/*.ejs'
     ],
-    plugins: ['karma-babel-preprocessor', 'karma-webpack', 'karma-mocha', 'karma-chrome-launcher'],
-    //plugins: ['karma-webpack'],
+    plugins: [
+        'karma-babel-preprocessor',
+        'karma-webpack',
+        'karma-mocha',
+        'karma-chrome-launcher',
+        'karma-firefox-launcher'
+    ],
     preprocessors: {
-        'public/scripts/Launcher.js': ['webpack'],
         'test/**/*.js': ['webpack']
-        //'public/**/*.js': ['webpack'],
-        //'public/**/*.ejs': ['webpack']
     },
     webpack: require("./webpack.config.js"),
     babelPreprocessor: {
@@ -25,17 +25,12 @@ module.exports = function(config) {
             presets: ['es2015']
         }
     },
-    client: {
-        mocha: {
-            opts: 'test/mocha.opts'
-        }
-    },
     reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox'],
     singleRun: true,
     concurrency: Infinity
   })
