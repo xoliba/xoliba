086bb61091fe072f3636d8a4b43d64366998b9e8
Eero Pukkila <eeropukkila@hotmail.com>
2017-06-08 15:31:38 +0300

    Play button starts a new game

--- public/scripts/Launcher.js
+++ public/scripts/Launcher.js
@@ -17,7 +17,17 @@ drawTable(app.stage);
 app.renderer.render(app.stage);
 app.renderer.backgroundColor = 0xE5E3DF;
 
-//let result = confirm("Choose your color\nThis is ugly. Needs to be changed\nOK = red, cancel = blue");
 
 var game = new Game(app);
+
+function startNewGame(){
+    var game = new Game(app);
+}
+
+var button = document.getElementById("Play");
+if(button.addEventListener){
+    button.addEventListener("click", function(){ var game = new Game(app); }, false);
+} else if (button.attachEvent){
+    button.attachEvent("onclick", function(){ var game = new Game(app); });
+}
 //game.start(); ?
