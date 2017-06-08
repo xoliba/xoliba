ece397ce320643c4772f8631e50d90cd877c18ed
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-08 11:09:40 +0300

    mocked game properly

--- test/GameTest.js
+++ test/GameTest.js
@@ -1,21 +1,21 @@
-import { MockBoard } from './mock/MockBoard.js';
-import { MockWebsocket } from './mock/MockWebsocket.js';
+import { Board } from '../public/scripts/Board.js';
+import { AiSocket } from '../public/scripts/Websocket.js';
 import { Game } from '../public/scripts/Game.js';
 import * as PIXI from 'pixi.js';
 
 describe('Game', () => {
 
     let game;
-/*
+
     beforeEach(() => {
         game = new Game(new PIXI.Application(20, 20, {view: document.getElementById("gameboard")}));
-        game.board = new MockBoard();
-        game.socket = new MockWebsocket();
+        //game.board = td.object('Board');
+        game.socket = td.object('AiSocket');
     });
 
     it('Ending turn works', function() {
         game.changeTurn();
         console.log("HALOo" + game.redPoints + " " + game.bluePoints);
-    });*/
+    });
 
 });
