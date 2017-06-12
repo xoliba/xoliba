8598d2c47f28b3907649c1ae3a1ba9334ad9628d
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-12 16:19:39 +0300

    refactored Launcher slightly

--- public/scripts/Launcher.js
+++ public/scripts/Launcher.js
@@ -1,6 +1,6 @@
 import { scale, drawTable } from './Draw.js';
 import * as PIXI from 'pixi.js';
-import { Game } from './Game';
+import { Game } from './Game.js';
 
 var app = new PIXI.Application(scale(), scale(), {view: document.getElementById("gameboard")});
 var game;
@@ -23,49 +23,32 @@ var redButton = document.getElementById("playAsRed");
 var undoButton = document.getElementById("undo");
 
 if(blueButton.addEventListener){
-    blueButton.addEventListener("click",
-        function(){
-            var score = document.getElementById("scorelimit").value;
-            document.getElementById("newGamePopUp").style.display = "none";
-            for (var i = app.stage.children.length - 1; i >= 1; i--) { app.stage.removeChild(app.stage.children[i]); };
-            game = new Game(app, -1, score);
-        } , false);
+    blueButton.addEventListener("click", () => { doStuff(app, game) }, false);
 } else if (blueButton.attachEvent){
-    blueButton.attachEvent("onclick",
-        function(){
-            var score = document.getElementById("scorelimit").value;
-            document.getElementById("newGamePopUp").style.display = "none";
-          for (var i = app.stage.children.length - 1; i >= 1; i--) { app.stage.removeChild(app.stage.children[i]); };
-            game = new Game(app, -1, score);
-        });
+    blueButton.attachEvent("onclick", () => { doStuff(app, game) });
 }
 
 if(redButton.addEventListener){
-    redButton.addEventListener("click",
-        function(){
-            var score = document.getElementById("scorelimit").value;
-            document.getElementById("newGamePopUp").style.display = "none";
-            for (var i = app.stage.children.length - 1; i >= 1; i--) { app.stage.removeChild(app.stage.children[i]); };
-            game = new Game(app, 1, score);
-        } , false);
+    redButton.addEventListener("click", () => { doStuff(app, game) }, false);
 } else if (redButton.attachEvent){
-    redButton.attachEvent("onclick",
-        function(){
-            var score = document.getElementById("scorelimit").value;
-            document.getElementById("newGamePopUp").style.display = "none";
-            for (var i = app.stage.children.length - 1; i >= 1; i--) { app.stage.removeChild(app.stage.children[i]); };
-            game = new Game(app, 1, score);
-        });
+    redButton.attachEvent("onclick", () => { doStuff(app, game) });
 }
 
 if(undoButton.addEventListener){
     undoButton.addEventListener("click",
-        function(){
+        () => {
             game.undo();
         } , false);
 } else if (undoButton.attachEvent){
     undoButton.attachEvent("onclick",
-        function(){
+        () => {
             game.undo();
         });
+}
+
+function doStuff(app, game) {
+    var score = document.getElementById("scorelimit").value;
+    document.getElementById("newGamePopUp").style.display = "none";
+    for (var i = app.stage.children.length - 1; i >= 1; i--) { app.stage.removeChild(app.stage.children[i]); };
+    game = new Game(app, -1, score);
 }
