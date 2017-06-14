2edbbd426e9592a9a9bf2a5e236b75ee3d76922e
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-14 16:14:30 +0300

    changed lets to vars in boardactiontests

--- test/BoardActionsTest.js
+++ test/BoardActionsTest.js
@@ -39,8 +39,8 @@ describe('BoardActions', () => {
     });
 
     describe('Hitting the stones in triangle work', () => {
-      let temp;
-      let table;
+      var temp;
+      var table;
       it('Legit moves work', () => {
         table = c(fullBoard);
         actions = new BoardActions();
@@ -52,8 +52,8 @@ describe('BoardActions', () => {
                   [1, 1, 1, 1, 1, 1, 1],
                  [-2, 1, 1, 1, 1, 1, -2]];
         assert.equal(actions.hitStones(3, 0, 0, 3, 3, 6, table), 2);
-        for(let i=0; i<table.length; i++) {
-          for(let j=0; j<table[i].length; j++) {
+        for(var i=0; i<table.length; i++) {
+          for(var j=0; j<table[i].length; j++) {
             assert.equal(table[i][j], temp[i][j]);
           }
         }
@@ -67,8 +67,8 @@ describe('BoardActions', () => {
                   [1, 1, 1, 1, 0, 0, 1],
                  [-2, 1, 1, 1, 1, 1, -2]];
         assert.equal(actions.hitStones(0, 5, 6, 5, 3, 2, table), 2);
-        for(let i=0; i<table.length; i++) {
-          for(let j=0; j<table[i].length; j++) {
+        for(var i=0; i<table.length; i++) {
+          for(var j=0; j<table[i].length; j++) {
             assert.equal(table[i][j], temp[i][j]);
           }
         }
@@ -82,8 +82,8 @@ describe('BoardActions', () => {
                   [1, 1, 1, 1, 1, 1, 1],
                  [-2, 1, 1, 1, 1, 1, -2]];
         assert.equal(actions.hitStones(3, 2, 3, 4, 2, 3, table), 2);
-        for(let i=0; i<table.length; i++) {
-          for(let j=0; j<table[i].length; j++) {
+        for(var i=0; i<table.length; i++) {
+          for(var j=0; j<table[i].length; j++) {
             assert.equal(table[i][j], temp[i][j]);
           }
         }
@@ -97,8 +97,8 @@ describe('BoardActions', () => {
                   [1, 1, 0, 0, 0, 1, 1],
                  [-2, 1, 0, 0, 0, 1, -2]];
         assert.equal(actions.hitStones(7, 3, 5, 5, 5, 1, table), 2);
-        for(let i=0; i<table.length; i++) {
-          for(let j=0; j<table[i].length; j++) {
+        for(var i=0; i<table.length; i++) {
+          for(var j=0; j<table[i].length; j++) {
             assert.equal(table[i][j], temp[i][j]);
           }
         }
@@ -114,8 +114,8 @@ describe('BoardActions', () => {
                   [1, 1, 1, 1, 1, 1, 1],
                  [-2, 1, 1, 1, 1, 1, -2]];
         assert.equal(actions.hitStones(0, 0, 6, 0, 0, 6, table), false);
-        for(let i=0; i<table.length; i++) {
-          for(let j=0; j<table[i].length; j++) {
+        for(var i=0; i<table.length; i++) {
+          for(var j=0; j<table[i].length; j++) {
             assert.equal(table[i][j], temp[i][j]);
           }
         }
@@ -123,10 +123,10 @@ describe('BoardActions', () => {
     });
 
     function c(array) { //copy the array
-      let rArray = new Array(7);
-      for (let i = 0; i < 7; i++) {
+      var rArray = new Array(7);
+      for (var i = 0; i < 7; i++) {
           rArray[i] = [];
-          for (let j = 0; j < 7; j++) {
+          for (var j = 0; j < 7; j++) {
               rArray[i][j] = array[i][j];
           }
       }
