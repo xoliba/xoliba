9dd1e5ee0b6b1c34d49b8a0337e01bfe5053aa29
Eero Pukkila <eeropukkila@hotmail.com>
2017-06-14 16:32:20 +0300

    Console works properly on wide screens

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,6 +2,7 @@ window.$ = require('jquery');
 require('../menuScripts/sidenav.js');
 require('../menuScripts/newgamepopup.js');
 require('../menuScripts/submenu.js');
+require('../menuScripts/console.js');
 document.write(require('../partials/sidenav.html'));
 document.write(require('../partials/left.html'));
 document.write(require('../partials/canvas.html'));
