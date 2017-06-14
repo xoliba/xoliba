8ec55a162babe39d809042fc64149fe19eb568fc
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-14 15:34:16 +0300

    made a test for ending round

--- test/GameTest.js
+++ test/GameTest.js
@@ -17,6 +17,13 @@ describe('Game', () => {
     let validate;
     let infoConsole;
     let UIUpdater;
+    let table = [[-2, 0, 0, 0, 0, 1, -2],
+                [0, 1, 1, 0, 1, 1, 0],
+                [0, 1, 0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 1],
+                [0, 1, 0, 0, 0, 0, 0],
+                [1, 0, 1, 0, 1, 1, 1],
+                [-2, 1, 1, 0, 1, -1, -2]];
 
     beforeEach(() => {
         game = new Game(new PIXI.Application(20, 20, {view: document.getElementById("gameboard")}));
@@ -25,7 +32,7 @@ describe('Game', () => {
         validate = td.object('Validate');
         UIUpdater = td.object('UIUpdater');
 
-        td.when(board.gameboardTo2dArray()).thenReturn(new Array());
+        td.when(board.gameboardTo2dArray()).thenReturn(table);
         //td.when(validate.isMovesAvailable()).thenReturn(true);
 
         game.board = board;
@@ -42,7 +49,7 @@ describe('Game', () => {
         game.changeTurn();
 
         td.verify(board.gameboardTo2dArray());
-        td.verify(socket.sendTable([], 1));
+        td.verify(socket.sendTable(table, 1));
     });
 
     it('sets the turn correctly at first turn', () => {
@@ -53,16 +60,21 @@ describe('Game', () => {
         assert.equal(game.turn, 1);
     });
 
-    /*it('starts a new round when there are no moves available', () => {
-        var startNewRound = td.function('startNewRound');
+    it('starts a new round when there are no moves available and round has been skipped', () => {
+        //var startNewRound = td.function('startNewRound');
         var updatePoints = td.function('updatePoints');
-        game.startNewRound = startNewRound;
+        //game.startNewRound = startNewRound;
         game.updatePoints = updatePoints;
 
-        td.when(validate.isMovesAvailable()).thenReturn(false);
+        game.turn = 1;
+        game.roundskipped = 1;
+        game.aiColor = -1;
+
+        td.when(validate.isMovesAvailable(1, [])).thenReturn(false);
 
         game.checkIfRoundEnds();
 
-        td.verify(startNewRound());
-    });*/
+        td.verify(board.generateStartingBoard());
+        td.verify(socket.sendStartRound(table, -1));
+    });
 });
