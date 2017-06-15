01cdae2e7f9a72ae6589163ddc7c43fcea960330
Terppa <terosoi@yahoo.co.uk>
2017-06-15 10:10:18 +0300

    added more boardtests

--- test/BoardTest.js
+++ test/BoardTest.js
@@ -1,32 +1,54 @@
-let assert = require('assert');
+var assert = require('assert');
 //var BoardClass = require('../public/scripts/Board.js');
 import { Board } from '../public/scripts/Board.js';
 import { TurnHandler } from '../public/scripts/TurnHandler.js';
+import { Game } from '../public/scripts/Game.js';
+import { BoardActions } from '../public/scripts/logic/BoardActions.js';
+import { Validations } from '../public/scripts/logic/Validations.js';
 import * as PIXI from 'pixi.js';
 
 describe('Board', () => {
 
+    var game;
     var board;
+    var actions;
+    var validate;
+    var turnhandler;
+    var table = [[-2, 0, -1, -1, 0, 1, -2],
+                [0, -1, -1, 0, 1, 1, 0],
+                [-1, 1, 0, 1, 1, 1, -1],
+                [0, -1, -1, -1, -1, 0, 1],
+                [0, 1, -1, -1, 0, -1, -1],
+                [1, -1, 1, -1, 1, 1, 1],
+                [-2, 1, 1, 0, -1, 1, -2]];
 
     beforeEach(() => {
         //board = new BoardClass.Board();
-        board = new Board(new PIXI.Application(20, 20, {view: document.getElementById("gameboard")}), td.object(['TurnHandler']));
+        game = new Game(new PIXI.Application(20, 20, {view: document.getElementById("gameboard")}));
+        board = td.object('Board');
+        game.board = board;
+        turnhandler = new TurnHandler(board, game);
+        board = new Board(new PIXI.Application(20, 20, {view: document.getElementById("gameboard")}), turnhandler);
+        actions = td.object('BoardActions');
+        validate = td.object('Validate');
+
+        board.validate = validate;
     });
 
 
-    it('is right size (and finding stones work)', function() {
+    it('is right size (and finding stones work)', () => {
         assert.equal(board.gameboardTo2dArray().length, 7);
-        for(let i=0; i<board.gameboardTo2dArray().length; i++) {
+        for(var i=0; i<board.gameboardTo2dArray().length; i++) {
             assert.equal(board.gameboardTo2dArray()[i].length, 7);
         }
     });
 
-    it('correct number of stones', function() {
-        let reds = 0;
-        let blues = 0;
-        let whites = 0;
-        for(let i = 0; i < board.gameboardTo2dArray().length; i++) {
-            for(let j = 0; j < board.gameboardTo2dArray()[i].length; j++) {
+    it('correct number of stones', () => {
+        var reds = 0;
+        var blues = 0;
+        var whites = 0;
+        for(var i = 0; i < board.gameboardTo2dArray().length; i++) {
+            for(var j = 0; j < board.gameboardTo2dArray()[i].length; j++) {
                 if (board.gameboardTo2dArray()[i][j] === 1) {
                     reds++;
                 } else if (board.gameboardTo2dArray()[i][j] === -1) {
@@ -41,11 +63,11 @@ describe('Board', () => {
         assert.equal(whites, 11);
     });
 
-    it('swapping works', function() {
+    it('swapping works', () => {
         //his swaps only 5 stones: so there is (theoretical) chance it will swap
         //same colored stones and the test will pass.
-        let table = board.gameboardTo2dArray();
-        let temp;
+        var table = board.gameboardTo2dArray();
+        var temp;
         temp = table[1][1];
         table[1][1] = table[1][2];
         table[1][2] = temp;
@@ -66,15 +88,24 @@ describe('Board', () => {
         table[5][1] = table[5][2];
         table[5][2] = temp;
         board.swap(board.findStone(5, 1), board.findStone(5, 2));
-        for(let i=0; i<board.gameboardTo2dArray().length; i++) {
-            for(let j=0; j<board.gameboardTo2dArray()[i].length; j++) {
+        for(var i=0; i<board.gameboardTo2dArray().length; i++) {
+            for(var j=0; j<board.gameboardTo2dArray()[i].length; j++) {
                 assert.equal(board.gameboardTo2dArray()[i][j], table[i][j]);
             }
         }
     });
 
-    it.skip('Hitting stones work', function() {
-        //TODO
+    it('Hitting stones work', () => {
+        var gameboardTo2dArray = td.function('gameboardTo2dArray');
+        board.gameboardTo2dArray = gameboardTo2dArray;
+        td.when(board.gameboardTo2dArray()).thenReturn(table);
+        assert.equal(board.hitStones(0, 5, 3, 2, 6, 5), 2);
     });
-    
+
+    it('Starting turn works', () => {
+        var gameboardTo2dArray = td.function('gameboardTo2dArray');
+        board.gameboardTo2dArray = gameboardTo2dArray;
+        td.when(board.gameboardTo2dArray()).thenReturn(table);
+        assert.equal(board.startingTurn(), -1);
+    });
 });
