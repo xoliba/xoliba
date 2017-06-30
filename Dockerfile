da5ba2a1fbd82b5d141d54e74b585695b10e7108
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-30 17:12:01 +0300

    testing aws in bar

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ EXPOSE 5000
 RUN apk update
 RUN apk add openssl
 
+#ADD public /xoliba/public/
+#ADD server.js /xoliba/server.js
+#ADD test /xoliba/test/
+#ADD package.json /xoliba/package.json
+#ADD webpack.config.js /xoliba/webpack.config.js
+
 RUN wget https://github.com/xoliba/xoliba/archive/master.zip -O front.zip; unzip front.zip; rm front.zip
 #RUN wget https://github.com/xoliba/xoliba-ai/archive/master.zip -O AI.zip; unzip AI.zip; rm AI.zip
 
--- awsUpload.sh
+++ awsUpload.sh
@@ -2,5 +2,5 @@
 
 eval $(aws ecr get-login --no-include-email --region us-east-2)
 docker build -t xoliba-front-aws .
-docker tag xoliba-front-aws:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/xoliba-docker-repo:front
-docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/xoliba-docker-repo:front
+docker tag xoliba-front-aws:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/xoliba-docker-repo:front1
+docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/xoliba-docker-repo:front1
