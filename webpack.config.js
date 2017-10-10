43b83c93a8214d200d09827955c279c9ab550a8b
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-10-10 12:31:04 +0300

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
 	        options: {
-            	presets: ['babel-preset-es2015'],
+            	presets: ['babel-preset-env'],
                 plugins: ['istanbul']
           }
         }  
